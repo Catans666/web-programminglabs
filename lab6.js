379c997719fd292fdea0133e26f565fac834fe37
Kopylov-kirill <kirill.kopylov.1@mail.ru>
2022-11-22 10:50:40 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -39,7 +39,36 @@ function setEnglish() {
 
     document.getElementById('ok-button').value = 'ok';
 
-
     document.getElementById('reset-button').value = 'reset';
-   
+
 }
+
+function showAlert() {
+let userName = document.getElementById('input-name').value;
+let userSurname = document.getElementById('input-surname').value;
+let userAge = document.getElementById('input-age').value;
+alert('Привет, ' + userName + '' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+let r = Math.round(Math.random()*255);
+let g = Math.round(Math.random()*255);
+let b = Math.round(Math.random()*255);
+let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+document.getElementById('user-data').style.background = color;
+}
+
+function showAlert2() {
+alert ('Привет! Это Имя и Фамилия студента');
+let r = Math.round(Math.random()*255);
+let g = Math.round(Math.random()*255);
+let b = Math.round(Math.random()*255);
+let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+document.getElementById('student').style.background = color;
+}
+
+function showAlert3() {
+    alert ('Привет! Это Факультет, группа, курс и год!');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.background = color;
+    }
