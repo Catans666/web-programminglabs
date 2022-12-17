432d5e00703f21753d795fd7ae66f866545c9a04
Kopylov-kirill <kirill.kopylov.1@mail.ru>
2022-12-17 10:09:24 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -11,9 +11,16 @@ function showDaysCount(parasm) {
     let daysCount = (today - birthday)/1000/60/60/24;
     let result = document.getElementById('result');
     daysCount = Math.floor(daysCount); // округляем в меньшую сторону
-    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;   
+    result.innerHTML = 'С даты рождения прошло дней:' + daysCount;   
 }
 
-
-
 window.addEventListener('load', ShowDate);
+ 
+function showTime() {
+    let outTime = document.getElementById('time');
+    let currentTime = new Date();
+    outTime.innerHTML = currentTime.toLocaleTimeString('ru');
+}
+
+window.addEventListener('load', showTime);
+setInterval(showTime, 1000); 
