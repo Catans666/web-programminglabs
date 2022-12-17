3bcfd15ad365e1695c2ee1de5edbe5532ccb789f
Kopylov-kirill <kirill.kopylov.1@mail.ru>
2022-12-17 09:58:16 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,4 +4,16 @@ let today = new Date ();
 out.innerHTML = today.toLocaleDateString ();
 }
 
+function showDaysCount(parasm) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount); // округляем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;   
+}
+
+
+
 window.addEventListener('load', ShowDate);
