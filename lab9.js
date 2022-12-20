25f4d4e036cb2b156484cbf90ecdb66cc127e574
Kopylov-kirill <kirill.kopylov.1@mail.ru>
2022-12-20 09:31:49 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,10 @@ function clickMe(button) {
     else
         button.innerHTML = 'Щелчок';
 }
+
+function clickMe2(button) {
+    if(button.innerHTML == 'Копылов')
+        button.innerHTML = 'Кирилл';
+    else
+        button.innerHTML = 'Копылов';
+}
