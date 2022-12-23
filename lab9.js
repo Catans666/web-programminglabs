a125c9271b64a2866dc5e5d789f5e05402af6607
Catans666 <114455373+Catans666@users.noreply.github.com>
2022-12-23 20:06:21 +0700

    Изменение

--- lab9.js
+++ lab9.js
@@ -5,7 +5,6 @@ function clickMe(button) {
         button.innerHTML = 'Щелчок';
 }
 
-
 function clickMe2(button) {
     if(button.innerHTML == 'Копылов')
         button.innerHTML = 'Кирилл';
@@ -15,7 +14,7 @@ function clickMe2(button) {
 
 function clickMe3(button)
 { 
-    if(button.innerHTML == 'Нажми меня')
+    if(button.innerHTML == 'Нажми меня') 
         button.innerHTML = 'Ещё раз нажми';
     else if (button.innerHTML == 'Ещё раз нажми')
         button.innerHTML = 'Ещё';
@@ -26,10 +25,39 @@ function clickMe3(button)
 }
 
 function showCoords(event) {
-    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     let coords = document.querySelector('#coords');
     coords.addEventListener('mousemove', showCoords);
+})
+function showFocus(event) {
+    event.target.style.color = 'blue';
+}
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type="text"]');
+    texts[0].addEventListener('focus', showFocus);
+    texts[1].addEventListener('focus', showFocus);
+    texts[2].addEventListener('focus', showFocus);
+});
+function showBlur(event) {
+    event.target.style.color = 'black';
+} 
+window.addEventListener('DOMContentLoaded', function() {
+let texts = document.querySelectorAll('input[type="text"]'); 
+texts[0].addEventListener('blur', showBlur);
+texts[1].addEventListener('blur', showBlur);
+texts[2].addEventListener('blur', showBlur);
+});
+
+function showOutput(event){
+    document.getElementById('output').innerHTML = event.target.value;
+}
+window.addEventListener('DOMContentLoaded', function() {
+let texts = document.querySelectorAll('input[type="text"]');
+    texts[0].addEventListener('change', showOutput);
+    texts[1].addEventListener('change', showOutput);
+    texts[2].addEventListener('change', showOutput);
 });
